a2825093eefbec964a312e08015184fa3e0690f3
abunaim34 <farhanadnanfarabi@gmail.com>
2026-05-19 20:48:52 +0600

    Auth middleware validation

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,4 +1,7 @@
 import type { NextFunction, Request, Response } from "express";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import config from "../config";
+import { pool } from "../DB";
 
 
 const auth = () => {
@@ -13,6 +16,27 @@ const auth = () => {
             })
         }
 
+        const decoded = jwt.verify(token as string, config.secret as string) as JwtPayload
+
+        const userData = await pool.query(`
+            FROM * users WHERE email=$1
+            `, [decoded.email])
+
+        const user = userData.rows[0]
+
+        if (userData.rows.length === 0) {
+            res.status(404).json({
+                message: "User not found!",
+                data: {}
+            })
+        }
+
+        if(!user.is_active){
+            res.status(403).json({
+                message: "forbidden",
+                data: {}
+            })
+        }
         next()
     }
 }
